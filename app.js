ee8af7faef40dadb8431c4c0a97bbc1a026a2286
aamir <aamirchanna1172gmail.com>
2024-05-10 07:21:14 -0700

    added condition

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 function tracker (){
-    var input = +document.getElementById('inputBox').value;
-    var less = +document.getElementById('inputBox2').value;
+    var input = document.getElementById('inputBox').value;
+    var less = document.getElementById('inputBox2').value;
    
     var income = document.getElementById('incomeResult');
     var expense = document.getElementById('expenseResult')
     var savings = document.getElementById('savingsResult')
       
+    if(isNaN(input) || isNaN(less) || input == "" || less == "" ){
+        alert("Field is Empty")
+      return;   
+    } else {     
       var addIncome = input;
       income.innerHTML = 'Your Salary is Rs. ' + addIncome ;
    
@@ -13,6 +17,6 @@ function tracker (){
       expense.innerHTML = 'Your Expensis is Rs. ' + addExp;
       
       var showRemain = addIncome - addExp;
-      savings.innerHTML = 'Your Saving is Rs. ' + showRemain;       
-   }
+      savings.innerHTML = 'Your Saving is Rs. ' + showRemain;
+   }}
    
